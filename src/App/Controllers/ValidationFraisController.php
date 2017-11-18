519429a488dcdc999f8167283804067c036480b0
Thomas Cianfarani <thomas.ci83@gmail.com>
2017-11-18 20:58:27 +0100

    Corrections de bugs sur l'affichage des fiches de frais forfait.

--- src/App/Controllers/ValidationFraisController.php
+++ src/App/Controllers/ValidationFraisController.php
@@ -35,7 +35,10 @@ class ValidationFraisController {
     public function index() {
         // Si on accède à la page pour le première fois, on génère la vue avec les infos basiques
         if (!isset($_POST['lstVisiteurs'])) {
+            // On récupère les mois pour lesquelles le premier visiteur de la liste visiteur a une fiche de frais
             $this->lesMois = $this->db->getLesMoisDisponibles($this->lesVisiteurs[0]["id"]);
+
+            // Création de la vue
             View::make('validationFrais.twig', array('lesMois' => $this->lesMois,
                 'moisASelectionner' => $this->lesMois[0],
                 'lesVisiteurs' => $this->lesVisiteurs,
@@ -43,15 +46,25 @@ class ValidationFraisController {
                 'lesFraisForfait' => $this->db->getLesFraisForfait($this->lesVisiteurs[0]["id"], $this->lesMois[0]["mois"])
             ));
         } else {
+            // On récupère les mois pour lesquelles le visiteur choisi a une fiche de frais
             $this->lesMois = $this->db->getLesMoisDisponibles($_POST['lstVisiteurs']);
+
+            // Vérifications sur l'existence du mois passé en post dans la liste des mois du visiteur choisi
+            $moisChoisi = $this->lesMois[0]["mois"];
+            for ($i = 0; $i < count($this->lesMois); $i++) {
+                if (in_array($_POST['lstMois'], $this->lesMois[$i])) {
+                    $moisChoisi = $_POST['lstMois'];
+                }
+            }
+
+            // Création de la vue
             View::make('validationFrais.twig', array('lesMois' => $this->lesMois,
                 'moisASelectionner' => $_POST['lstMois'],
                 'lesVisiteurs' => $this->lesVisiteurs,
                 'visiteurASelectionner' => $_POST['lstVisiteurs'],
-                'lesFraisForfait' => $this->db->getLesFraisForfait($_POST['lstVisiteurs'], $_POST['lstMois'])
+                'lesFraisForfait' => $this->db->getLesFraisForfait($_POST['lstVisiteurs'], $moisChoisi)
             ));
         }
     }
-}
 
-?>
+}
